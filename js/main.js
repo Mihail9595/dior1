0032e764644645c040a8f0c4e8a6ae59df5e96f7
Mihail9595 <mihailkokskokin@gmail.com>
2024-10-20 14:29:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const swiperTop = new Swiper('.top-swiper', {
+    // Optional parameters
+ 
+    effect: 'fade',
+    autoplay:{
+        delay:5500,
+        disableOnInteraction: false,
+    },
+    
+    pagination: {
+      el: ".swiper-pagination",
+    },
+     
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+  const swiperAbout = new Swiper(".about-slider", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+
+  document.querySelectorAll('.accordeon-triger').forEach((item) => {
+    item.addEventListener('click', () => {
+      item.parentNode.classList.toggle('accordeon-item-active') 
+      
+    })
+  });
